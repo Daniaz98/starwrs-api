5a912d559111ce24d781a54044257f122a04339f
Daniel Azara <danielazara04@gmail.com>
2024-08-04 18:58:14 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+const Film = mongoose.model("Film", {
+  title: String,
+  description: String,
+  image_url: String,
+  trailer_url: String,
+});
+
+app.get("/", async (req, res) => {
+  const films = await Film.find();
+  return res.send(films);
+});
+
+app.get("/films/search", async (req, res) => {
+  const name = req.query.name;
+  const films = await Film.find({ title: new RegExp(name, "i") });
+
+  return res.send(films);
+});
+
+app.delete("/:id", async (req, res) => {
+  const film = await Film.findByIdAndDelete(req.params.id);
+  return res.send(film);
+});
+
+app.put("/:id", async (req, res) => {
+  const film = await Film.findByIdAndUpdate(req.params.id, {
+    title: req.body.title,
+    description: req.body.description,
+    image_url: req.body.image_url,
+    trailer_url: req.body.trailer_url,
+  });
+
+  return res.send(film);
+});
+
+app.post("/", async (req, res) => {
+  const film = new Film({
+    title: req.body.title,
+    description: req.body.description,
+    image_url: req.body.image_url,
+    trailer_url: req.body.trailer_url,
+  });
+
+  await film.save();
+  return res.send(film);
+});
+
+app.listen(port, () => {
+  mongoose.connect(
+    "mongodb+srv://user1:2Z0MhzlgngPSuQ0S@starwarsdb.6x594yw.mongodb.net/?retryWrites=true&w=majority&appName=starwarsdb"
+  );
+
+  console.log(`Funcionando na porta ${port}`);
+});
